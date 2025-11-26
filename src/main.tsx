4e6b8e3ce38b29c6a01c0bcefce08f67656bf6c5
Hrelea1 <hrelea001@gmail.com>
2025-11-26 13:31:58 +0200

    Refactor main.tsx to use BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
-
-import App from "./App.tsx";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
- import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename="/Experium">
     <App />
   </BrowserRouter>
 );
-
