f3163e216a1514af37032ebf7a17e26ab7364f20
Hrelea1 <hrelea001@gmail.com>
2025-11-26 13:11:28 +0200

    Wrap App component with BrowserRouter
    poate reușesc

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
+
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+ import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter basename="/Experium">
+    <App />
+  </BrowserRouter>
+);
+
